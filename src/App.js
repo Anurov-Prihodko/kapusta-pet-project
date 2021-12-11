d2f1b8dccd7a5a33c9a2310305a65f6153df0d9d
Mariia Ros <rosm0099@gmail.com>
2021-12-11 23:54:48 +0100

    test for Summary inserted into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import Container from './components/Container';
 import LoginView from './views/LoginView';
 import HomeView from './views/HomeView';
+import { Summary } from './components/Summary';
+
+const exampleSummary = [
+  { month: 0, sum: 10000.0 },
+  { month: 1, sum: 10000.0 },
+  { month: 2, sum: 100.0 },
+  { month: 3, sum: 10000.0 },
+  { month: 4, sum: 1000.0 },
+  { month: 5, sum: 10000.0 },
+  { month: 6, sum: 10000.0 },
+  { month: 7, sum: 10000.0 },
+  { month: 8, sum: 10000.0 },
+  { month: 9, sum: 10000.0 },
+  { month: 10, sum: 10000.0 },
+  { month: 11, sum: 10000.0 },
+];
 
 console.log(HomeView);
 
@@ -9,6 +25,7 @@ export default function App() {
     <Container>
       <LoginView />
       {/* <HomeView /> */}
+      <Summary data={exampleSummary} />
     </Container>
   );
 }
