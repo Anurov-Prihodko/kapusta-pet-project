9236b0e0e7bd7beb6289cb39edd5cd0e069000b4
Igor Klyopov <shoot8202@gmail.com>
2021-12-12 14:58:04 +0200

    adds basic validation

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,10 +1,40 @@
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import styles from './LoginForm.module.scss';
 import Icons from '../../Icons';
 import ButtonBasic from '../ButtonBasic/ButtonBasic';
+import { useState } from 'react';
 
 export default function LoginForm() {
+  const [isEnterActive, setIsEnterActive] = useState(true);
+  const [isRegisterActive, setIsRegisterActive] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const toggleEnterActiveBtn = () => {
+    setIsEnterActive(true);
+    setIsRegisterActive(false);
+  };
+
+  const toggleRegisterActiveBtn = () => {
+    setIsEnterActive(false);
+    setIsRegisterActive(true);
+  };
+
+  const onLoginFormSubmit = data => {
+    console.log(data); //for test
+  };
+
+  const requiredErrorEmail = errors?.password?.type === 'required';
+  const requiredErrorPassword = errors?.password?.type === 'required';
+
+  const passwordErrorMessage =
+    'пароль должен быть от 6 до 8 символов, содержать буквы латинского алфавита и хотя бы одну цифру';
+
   return (
     <section className={styles.login}>
       <h2 className="visuallyHidden">Регистрация и вход</h2>
@@ -24,36 +54,78 @@ export default function LoginForm() {
         Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
       </p>
 
-      <form className={styles.loginForm}>
+      <form
+        className={styles.loginForm}
+        onSubmit={handleSubmit(onLoginFormSubmit)}
+      >
         <label className={styles.inputLabel}>
           <span className={styles.inputLabelText}>
-            <span className={styles.requiredLabel}>*</span>Электронная почта:
+            {requiredErrorEmail && (
+              <span className={styles.requiredLabel}>*</span>
+            )}
+            Электронная почта:
           </span>
           <input
             type="email"
+            name="email"
             placeholder="your@email.com"
-            required
             className={styles.input}
+            {...register('email', {
+              required: true,
+              pattern: /^[^\s@]+@[^\s@]+$/,
+            })}
           />
+          <span className={styles.inputErrorMessage}>
+            {requiredErrorEmail && 'это обязательное поле'}
+            {errors?.email?.type === 'pattern' &&
+              'это не похоже на адрес эл. почты - проверьте правильность ввода'}
+          </span>
         </label>
-        <span className={styles.inputErrorMessage}></span>
+
         <label className={styles.inputLabel}>
           <span className={styles.inputLabelText}>
-            <span className={styles.requiredLabel}>*</span> Пароль:
+            {requiredErrorPassword && (
+              <span className={styles.requiredLabel}>*</span>
+            )}
+            Пароль:
           </span>
           <input
             type="text"
+            name="password"
             placeholder="Пароль"
-            required
             className={styles.input}
+            {...register('password', {
+              required: true,
+              minLength: 6,
+              maxLength: 8,
+              pattern: /^[a-zA-Z0-9]+$/,
+            })}
           />
+          <span className={styles.inputErrorMessage}>
+            {requiredErrorPassword && 'это обязательное поле'}
+            {errors?.password?.type === 'minLength' && passwordErrorMessage}
+            {errors?.password?.type === 'maxLength' && passwordErrorMessage}
+            {errors?.password?.type === 'pattern' && passwordErrorMessage}
+          </span>
         </label>
-        <span className={styles.inputErrorMessage}></span>
+
         <div className={styles.loginFormBtnGroup}>
-          <ButtonBasic type="submit" shadow active>
+          <ButtonBasic
+            type="submit"
+            shadow
+            active={isEnterActive}
+            name="enter"
+            onClick={toggleEnterActiveBtn}
+          >
             Войти
           </ButtonBasic>
-          <ButtonBasic type="submit" shadow>
+          <ButtonBasic
+            type="submit"
+            shadow
+            active={isRegisterActive}
+            name="register"
+            onClick={toggleRegisterActiveBtn}
+          >
             Регистрация
           </ButtonBasic>
         </div>
