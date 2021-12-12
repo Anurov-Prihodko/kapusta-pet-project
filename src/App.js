003e7ef0002fe195aee9b0decb5e21c0ffa5c6a0
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-12-12 21:07:55 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import Container from './components/Container';
 import LoginView from './views/LoginView';
 import HomeView from './views/HomeView';
-import { Summary } from './components/Summary';
-
-const exampleSummary = [
-  { month: 0, sum: 10000.0 },
-  { month: 1, sum: 10000.0 },
-  { month: 2, sum: 100.0 },
-  { month: 3, sum: 10000.0 },
-  { month: 4, sum: 1000.0 },
-  { month: 5, sum: 10000.0 },
-  { month: 6, sum: 10000.0 },
-  { month: 7, sum: 10000.0 },
-  { month: 8, sum: 10000.0 },
-  { month: 9, sum: 10000.0 },
-  { month: 10, sum: 10000.0 },
-  { month: 11, sum: 10000.0 },
-];
 
 console.log(HomeView);
 
@@ -24,8 +8,7 @@ export default function App() {
   return (
     <Container>
       <LoginView />
-      {/* <HomeView /> */}
-      <Summary data={exampleSummary} />
+      <HomeView />
     </Container>
   );
 }
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,9 +1,24 @@
 import { Summary } from '../components/Summary';
 
+const exampleSummary = [
+  { month: 0, sum: 10000.0 },
+  { month: 1, sum: 10000.0 },
+  { month: 2, sum: 100.0 },
+  { month: 3, sum: 10000.0 },
+  { month: 4, sum: 1000.0 },
+  { month: 5, sum: 10000.0 },
+  { month: 6, sum: 10000.0 },
+  { month: 7, sum: 10000.0 },
+  { month: 8, sum: 10000.0 },
+  { month: 9, sum: 10000.0 },
+  { month: 10, sum: 10000.0 },
+  { month: 11, sum: 10000.0 },
+];
+
 export default function HomeView() {
   return (
     <>
-      <Summary />
+      <Summary data={exampleSummary} />
     </>
   );
 }
