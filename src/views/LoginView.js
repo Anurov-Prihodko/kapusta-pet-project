c46c2d32e49ea84a0aaa90c6adf0aea2f4a8f8e2
Oleksandr220 <74794729+Oleksandr220@users.noreply.github.com>
2021-12-13 01:22:26 +0200

    add logout header-home

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,10 +1,12 @@
-import Header from '../components/Header';
+// import Header from '../components/Header';
+import HeaderHome from '../components/HeaderHome';
 import Main from '../components/Main';
 
 export default function LoginView() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <HeaderHome />
       <Main />
     </>
   );
