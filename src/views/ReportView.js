a57a05bd62e2906f61b81c84d134224e3e57bb54
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-12-15 09:50:56 +0200

    Добавил key на списки/мелкие правки

--- src/views/ReportView.js
+++ src/views/ReportView.js
@@ -19,7 +19,7 @@ export default function ReportView() {
           <MonthSelector month={month} year={year} />
         </div>
         <div className={s.balance_line}>
-          <BalanceReport incomes={123000} outcomes={234075} />
+          <BalanceReport incomes={234075} outcomes={123000} />
         </div>
       </div>
     </>
