d791e785b54819f814c9fa7baa17c1a44d0e35a4
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2021-12-17 09:25:15 +0200

    .

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,14 +1,54 @@
+import { useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+import ModalBody from '../ModalBody';
+import Modal from '../Modal';
+
 import './Balance.scss';
+import BalanceButton from './BalanceButton'
 
 export default function Balance() {
-    return(
+    
+      const [isModalOpen, setIsModalOpen] = useState(false);
+    const togleModal = () => setIsModalOpen(state => !state);
+    
+      useEffect(() => {
+    document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+      }, [isModalOpen]);
+    
+    const desctopOrLaptopSize = useMediaQuery({
+    query: '(min-width: 768px)',
+  });
+
+  const mobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
+    return (
         <div className={'balance-wrapper'}>
             <p className={'balance-title'}>Баланс:</p>
             <div className={'balance-wrap'}>
-                <p className={'balance-value'}>55 000.00 UAH</p>
-                <button className={'balance-btn'}>ПОДТВЕРДИТЬ</button>
+                <form>
+                    <label>
+                        <input className={'balance-value'} type="number" autoComplete='off' name="name" placeholder={'00.00 UAH'}/>
+                    </label>
+                </form>
+                <div>
+                {mobile && (
+                    <BalanceButton onClick={togleModal} />
+                )}
+                {desctopOrLaptopSize && (
+                    <BalanceButton onClick={togleModal} />
+                )}
                 </div>
+                {isModalOpen && (
+                    <Modal onClose={togleModal}>
+          <ModalBody onClose={togleModal}>
+            Вы уверены?
+          </ModalBody>
+        </Modal>
+                )}
+            </div>
         </div>
         
-    )
+    );
 }
