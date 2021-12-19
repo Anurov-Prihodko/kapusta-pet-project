b491064f514dafa1ee14d8ac76a33293e997e5d1
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2021-12-19 17:34:12 +0200

    jamping cabbage add

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import s from './Main.module.scss';
 import Icons from '../../Icons';
 import LoginForm from '../LoginForm/LoginForm';
 import Cole from '../Cole';
+import JampCabb from '../JumpingCabbage';
 
 export default function Main() {
   const user = useSelector(state => state.auth.user);
@@ -14,7 +15,9 @@ export default function Main() {
         <Icons name="union" className={s.hero__logo} />
         <h1 className={s.hero__title}>Smart Finance</h1>
       </div>
-      <div className={s.main}></div>
+      {/* <div className={s.main}></div> 
+      вместо этого элемента рендерим анимацию в  JampCabb  */}
+      <JampCabb />
       <LoginForm />
       {/* {!user && <LoginForm />} */}
     </div>
