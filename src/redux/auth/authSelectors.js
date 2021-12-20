e3d951ec640a09fe0297074f618ae4002e74304d
iborovikov <ivans.borovikovs@live.com>
2021-12-20 13:06:28 +0200

    добавил селекторы для баланса и поля balanceHasBeenSet

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -2,5 +2,7 @@ const getIsLoggedIn = state => state.auth.isLoggedIn;
 const getUserEmail = state => state.auth.user.email;
 const getUserToken = state => state.auth.user.token;
 const getIsChecksCurrentUser = state => state.auth.isChecksCurrentUser;
+const getBalance = state => state.auth.user.balance;
+const getBalanceHasBeenSet = state => state.auth.user.balanceHasBeenSet;
 
-export { getIsLoggedIn, getUserEmail, getUserToken, getIsChecksCurrentUser };
+export { getIsLoggedIn, getUserEmail, getUserToken, getIsChecksCurrentUser, getBalance, getBalanceHasBeenSet };
