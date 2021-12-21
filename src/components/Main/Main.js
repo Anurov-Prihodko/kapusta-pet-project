cb2d056ab867b4644829fa82520a91a5721f310a
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2021-12-21 18:40:50 +0200

    anmation background

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -10,14 +10,18 @@ export default function Main() {
 
   return (
     <div className={s.main__container}>
-      <Cole />
+      <div className={s.flayanimation}>
+        <Cole />
+      </div>
       <div className={s.hero}>
         <Icons name="union" className={s.hero__logo} />
         <h1 className={s.hero__title}>Smart Finance</h1>
       </div>
-      {/* <div className={s.main}></div> 
-      вместо этого элемента рендерим анимацию в  JampCabb  */}
-      <JampCabb />
+      <div className={s.kapustaflay}>
+        <div className={s.anim}>
+          <JampCabb />
+        </div>
+      </div>
       <LoginForm />
       {/* {!user && <LoginForm />} */}
     </div>
