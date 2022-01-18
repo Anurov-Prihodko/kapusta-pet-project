dbef8849231211fe1f28f931ece1e342e44a0989
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2022-01-18 21:37:46 +0200

    edits
    добавила скролл на мобильной версии

--- src/components/MobileInput/MobileInput.js
+++ src/components/MobileInput/MobileInput.js
@@ -21,7 +21,7 @@ import dateRequest from '../../services/dateRequest';
 import { getTransactionsAnnual } from '../../redux/summary/summaryOperations';
 import { formatInputValue } from '../../utils/formatInputValue';
 
-export default function MobileInput() {
+export default function MobileInput({ onClose }) {
   // eslint-disable-next-line no-unused-vars
   const [startDate, setStartDate] = useState(new Date());
   const [request, setRequest] = useState('');
@@ -36,6 +36,14 @@ export default function MobileInput() {
     dispatch(getExpenseByDate(dateRequest(startDate)));
     dispatch(getIncomseByDate(dateRequest(startDate)));
   }, [startDate, dispatch]);
+
+  useEffect(() => {
+    const button = document.getElementById('button-close');
+    button.addEventListener('click', onClose);
+    return () => {
+      button.removeEventListener('click', onClose);
+    };
+  }, [onClose]);
 
   const dateFormatter = date => {
     const options = {
@@ -121,80 +129,87 @@ export default function MobileInput() {
     }
   };
   return (
-    <div className={s.expininput}>
-      <div className={s.inputfirst}>
-        <input
-          value={request}
-          onChange={handleNameChange}
-          type="text"
-          className={s.expinplace}
-          placeholder="Описание товара"
-        />
-        {incomeStatus === false ? (
-          <>
-            <select
-              value={category}
-              onChange={changeSelect}
-              className={s.expinplace}
-            >
-              <option value="">Категория товара</option>
-              {expenseCategories
-                .filter(item => item.income === false)
-                .map(item => (
-                  <option key={item.category}>{item.category}</option>
-                ))}
-            </select>
-          </>
-        ) : (
-          <>
-            <select
-              value={category}
-              onChange={changeSelect}
-              className={s.expinplace}
-            >
-              <option value="">Категория товара</option>
-              {expenseCategories
-                .filter(item => item.income === true)
-                .map(item => (
-                  <option key={item.category}>{item.category}</option>
-                ))}
-            </select>
-          </>
-        )}
+    <div className={s.body}>
+      <div className={s.wraper}>
+        <button type="button" id="button-close" className={s.close__btn}>
+          <Icons name="close" className={s.close__btn_svg} />
+        </button>
       </div>
-      <div className={s.secondinput}>
-        <input
-          value={expenses}
-          onChange={handleNumbChange}
-          type="number"
-          placeholder="0"
-          className={s.expinplace}
-        />
-        <Icons
-          name="calculator"
-          width={20}
-          height={20}
-          className={s.iconCalculator}
-        />
-      </div>
-      <div className={s.btnGroup}>
-        <ButtonBasic
-          type="submit"
-          active={true}
-          name="enter"
-          onClick={handleSubmit}
-        >
-          Ввод
-        </ButtonBasic>
-        <ButtonBasic
-          type="submit"
-          active={false}
-          bordered={true}
-          name="clean"
-          onClick={onClear}
-        >
-          Очистить
-        </ButtonBasic>
+      <div className={s.expininput}>
+        <div className={s.inputfirst}>
+          <input
+            value={request}
+            onChange={handleNameChange}
+            type="text"
+            className={s.expinplace}
+            placeholder="Описание товара"
+          />
+          {incomeStatus === false ? (
+            <>
+              <select
+                value={category}
+                onChange={changeSelect}
+                className={s.expinplace}
+              >
+                <option value="">Категория товара</option>
+                {expenseCategories
+                  .filter(item => item.income === false)
+                  .map(item => (
+                    <option key={item.category}>{item.category}</option>
+                  ))}
+              </select>
+            </>
+          ) : (
+            <>
+              <select
+                value={category}
+                onChange={changeSelect}
+                className={s.expinplace}
+              >
+                <option value="">Категория товара</option>
+                {expenseCategories
+                  .filter(item => item.income === true)
+                  .map(item => (
+                    <option key={item.category}>{item.category}</option>
+                  ))}
+              </select>
+            </>
+          )}
+        </div>
+        <div className={s.secondinput}>
+          <input
+            value={expenses}
+            onChange={handleNumbChange}
+            type="number"
+            placeholder="0"
+            className={s.expinplace}
+          />
+          <Icons
+            name="calculator"
+            width={20}
+            height={20}
+            className={s.iconCalculator}
+          />
+        </div>
+        <div className={s.btnGroup}>
+          <ButtonBasic
+            type="submit"
+            active={true}
+            name="enter"
+            onClick={handleSubmit}
+          >
+            Ввод
+          </ButtonBasic>
+          <ButtonBasic
+            type="submit"
+            active={false}
+            bordered={true}
+            name="clean"
+            onClick={onClear}
+          >
+            Очистить
+          </ButtonBasic>
+        </div>
       </div>
     </div>
   );
