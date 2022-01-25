8cf4926b6df5007b333039a172082f436a56d202
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2022-01-25 12:31:44 +0200

    bagFix25_01

--- src/utils/useGoogleAuthLogin.js
+++ src/utils/useGoogleAuthLogin.js
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useSearchParam } from 'react-use';
 import { loginUserViaGoogle } from '../redux/auth/authOperations';
-import { createBrowserHistory } from 'history';
+import { useNavigate } from 'react-router-dom';
 
 const useGoogleAuthLogin = () => {
   const { pathname } = useLocation();
   const access_token = useSearchParam('access_token');
   const email = useSearchParam('email');
-  const history = createBrowserHistory();
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -19,9 +19,9 @@ const useGoogleAuthLogin = () => {
           email,
         }),
       );
-      history.push('/');
+      navigate('/');
     }
-  }, [access_token, email, pathname, dispatch, history]);
+  }, [access_token, email, pathname, dispatch, navigate]);
 };
 
 export default useGoogleAuthLogin;
